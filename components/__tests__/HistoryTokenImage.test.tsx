63b21b218d48b61eba07ae06fcfba2a279024cf6
3seater <threeseaterdesigns@gmail.com>
2026-09-13 20:01:18 -0400

    Suppress unused var lint error in test mock

--- components/__tests__/HistoryTokenImage.test.tsx
+++ components/__tests__/HistoryTokenImage.test.tsx
@@ -1,7 +1,7 @@
 import { act } from 'react';
 import { createRoot } from 'react-dom/client';
 import { expect, it, vi } from 'vitest';
-vi.mock('next/image', () => ({ default: ({ unoptimized: _u, ...props }: Record<string, unknown>) => <img {...props} /> }));
+vi.mock('next/image', () => ({ default: ({ unoptimized: _, ...props }: Record<string, unknown>) => <img {...props} /> })); // eslint-disable-line @typescript-eslint/no-unused-vars
 import { HistoryTokenImage } from '../HistoryTokenImage';
 
 it('keeps a slow logo mounted past ten seconds and shows it when loading finishes', async () => {
